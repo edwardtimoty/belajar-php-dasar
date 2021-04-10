85697eecd097be42cff6af25e670069b663bd3ea
Drawde <60484184+edwardtimoty@users.noreply.github.com>
2021-04-11 03:04:27 +0700

    belajar pengkondisian dan pengulangan
    9/4/2021

---
+++ 2. Struktur_Kendali/latihan1.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Dasar | Struktur Kendali | Latihan 1</title>
+	<style>
+		.warna-baris {
+			background-color: silver;
+		}
+	</style>
+</head>
+<body>
+    <table border="1" cellpadding="10" celspacing="0">
+        <!-- <tr>
+            <td>1,1</td>
+            <td>1,2</td>
+            <td>1,3</td>
+            <td>1,4</td>
+            <td>1,5</td>
+        </tr> -->
+		<?php
+		// 	for($i = 1; $i <= 3; $i++) {
+		// 		echo "<tr>";
+		// 		for($j = 1; $j < 5; $j ++) {
+		// 			echo "<td>$i,$j</td>";
+		// 		}
+		// 		echo "</tr>";
+		// 	} 
+		//  
+		// <?= == <?php echo
+		// : = {
+		// endfor; = }
+		for($i = 1; $i <= 5; $i++) :?>
+			<?php if($i % 2 == 1) : ?>
+			<tr class = "warna-baris">
+		<?php else : ?>
+			<tr>
+		<?php endif; ?> 
+			<?php for($j = 1; $j <= 5; $j++) : ?>
+			<td><?= "$i,$j"; ?></td>
+			<?php endfor; ?>
+		</tr>
+		<?php endfor; ?>
+    </table>
+</body>
+</html>
---
+++ 2. Struktur_Kendali/pengkondisian.php
@@ -0,0 +1,31 @@
+<?php 
+
+// Pengkondisian / Percabangan
+// IF ELSE
+// $a = 10;
+// if( $a < 20 ) {
+//     echo "benar";
+// } else {
+//     echo "salah";
+// }
+
+// IF ELSE IF ELSE
+$a = 30;
+if( $a < 20 ) {
+    echo "benar";
+} else if ($a == 20) {
+    echo "BINGO!";
+} else {
+    echo "salah";
+}
+// TERNARY
+
+// SWITCH
+
+
+
+
+
+
+
+?>
