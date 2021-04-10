ed635df0d0732a3fd1bfc26a3dcac383c064875c
Drawde <60484184+edwardtimoty@users.noreply.github.com>
2021-04-11 03:00:31 +0700

    belajar awal sintaks php
    9/4/2021

---
+++ 1. Sintaks_PHP/index.php
@@ -0,0 +1,68 @@
+<?php
+// standar output
+// echo , print = mencetak tulisan
+// echo "Hello World";
+// print  "Hello World";
+
+// print_r = khusus digunakan untuk mencetak isi array
+// print_r ("Hello World");
+
+// var_dump = melihat isi dari variabel akan tampil informasi biasanya untuk debugging
+// var_dump("Hello World");
+
+// Variabel
+// $nama = "Edward"
+
+//Operator aritmatika
+// + - / * %
+// $a = 10;
+// $b = 10;
+// echo $a + $b;
+
+
+// Penggabungan String / Concat
+// $nama_depan = "Edward";
+// $nama_blkg = "Timoty";
+// echo $nama_depan . " " . $nama_blkg;
+
+//Assignment
+// =, +=, -=, *=, /=, %=, .=
+// $a = 1;
+// $a += 5;
+// echo $a; a= a+5
+
+// Perbandingan (mengecek tidak dengan tipe data)
+// <, >, <=, >=, ==
+// var_dump(1 == "1");
+
+// identitas (mengecek tipe datanya harus sama)
+// ===, !==
+// var_dump(1 ==== "1");
+
+// Logika
+// && (dan), || (or), !(not)
+// $x = 10;
+// var_dump($x < 20 && $x %2 == 0); // harus keduanya bernilai true
+// var_dump($x < 20 || $x %2 == 0); //salah satu true
+
+
+
+
+
+
+
+
+?>
+
+<!-- <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Hello selamat datang </h1>
+</body>
+</html> -->
